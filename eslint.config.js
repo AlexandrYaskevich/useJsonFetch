e1692ba39b298ce243b268a8aee12da01f27ea42
AppVeyor <yask.alexandr@gmail.com>
2024-11-24 22:15:33 +0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,9 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.node,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -28,6 +30,7 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      "no-unused-vars": 0,
       'react/jsx-no-target-blank': 'off',
       "react/prop-types": "off",
       "react/jsx-uses-react": "error",   
